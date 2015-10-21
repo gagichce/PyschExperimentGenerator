b0873a1d789b5b36ad35d803efd450d8fedf9aa2
gagichce <gagichce@mcmaster.ca>
2015-10-20 23:16:25 -0400

    initial checkin of working project

---
+++ PsychExperimentGenerator/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace PsychExperimentGenerator
+{
+    class Program
+    {
+        private static int height = 3000;
+        private static int width = 4000;
+        private static int overlaySize = 1000;
+
+        private static Bitmap myBitmap;
+        static void Main(string[] args)
+        {
+
+
+            Enum.GetValues(typeof(Slide.Gender)).OfType<Slide.Gender>().ToList().ForEach(gender =>
+            {
+                Enum.GetValues(typeof(Slide.BodyType)).OfType<Slide.BodyType>().ToList().ForEach(bodyType =>
+                {
+                    Enumerable.Range(1, 16).ToList().ForEach(number =>
+                    {
+                        var imageToCompose = Image.FromFile(@"in\" + string.Format("{0}{1} {2}.jpg", (gender == Slide.Gender.Male ? "M" : "F"), number, (bodyType == Slide.BodyType.Body ? "Body" : "Face")));
+
+                        Enum.GetValues(typeof(Slide.ImageLocation)).OfType<Slide.ImageLocation>().ToList().ForEach(location =>
+                        {
+                            Enum.GetValues(typeof(Slide.ShapeType)).OfType<Slide.ShapeType>().ToList().ForEach(shape =>
+                            {
+                                var slide = new Slide
+                                {
+                                    ImageGender = gender,
+                                    ImageNumber = number,
+                                    Location = location,
+                                    Shape = shape,
+                                    Body = bodyType
+                                };
+
+                                int imageXoffset = 0;
+                                int imageYoffset = 0;
+                                int shapeXoffset = 0;
+                                int shapeYoffset = 0;
+
+                                switch (location)
+                                {
+                                    case Slide.ImageLocation.TopLeft:
+                                        shapeXoffset = width - overlaySize;
+                                        shapeYoffset = height - overlaySize;
+                                        break;
+                                    case Slide.ImageLocation.TopRight:
+                                        imageXoffset = width - overlaySize;
+                                        shapeYoffset = height - overlaySize;
+                                        break;
+
+                                    case Slide.ImageLocation.BottomLeft:
+                                        imageYoffset = height - overlaySize;
+                                        shapeXoffset = width - overlaySize;
+                                        break;
+                                    case Slide.ImageLocation.BottomRight:
+                                        imageXoffset = width - overlaySize;
+                                        imageYoffset = height - overlaySize;
+                                        break;
+                                }
+
+                                Image myBitmap = new Bitmap(width, height);
+
+                                using (Graphics g = Graphics.FromImage(myBitmap))
+                                {
+                                    g.FillRectangle(Brushes.White, new Rectangle(0, 0, width, height));
+
+                                    if (shape == Slide.ShapeType.Circle)
+                                        g.FillEllipse(Brushes.Black, new Rectangle(shapeXoffset, shapeYoffset, overlaySize, overlaySize));
+                                    else
+                                        g.FillRectangle(Brushes.Black, new Rectangle(shapeXoffset, shapeYoffset, 1000, 1000));
+
+                                    g.DrawImage(imageToCompose, new Rectangle(imageXoffset, imageYoffset, overlaySize, overlaySize));
+
+                                    myBitmap.Save(@"img\" + slide.FileName, ImageFormat.Jpeg);
+
+                                    myBitmap.Dispose();
+                                }
+                            });
+                        });
+                    });
+                });
+            });
+
+
+        }
+    }
+}
---
+++ PsychExperimentGenerator/Slide.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsychExperimentGenerator
+{
+    public class Slide
+    {
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+
+        public enum ImageLocation
+        {
+            TopLeft,
+            TopRight,
+            BottomLeft,
+            BottomRight
+        }
+
+        public enum ShapeType
+        {
+            Square,
+            Circle
+        }
+
+        public enum BodyType
+        {
+            Face,
+            Body
+        }
+
+        public ShapeType Shape { get; set; }
+        public Gender ImageGender { get; set; }
+        public ImageLocation Location { get; set; }
+        public BodyType Body { get; set; }
+        public int ImageNumber { get; set; }
+
+        public string FileName
+        {
+            get { return string.Format("{0}.{1}{2} ({3}) {4}.jpg", (Shape == ShapeType.Circle ? "C" : "S"), (ImageGender == Slide.Gender.Male ? "M" : "F"), (Body == BodyType.Body) ? "B" : "F", ImageNumber, GetCaps(Location)); }
+        }
+
+        private string GetCaps(ImageLocation location)
+        {
+            return String.Concat(Enum.GetName(typeof(ImageLocation), location).Where(l => char.IsUpper(l)));
+        }
+    }
+}
